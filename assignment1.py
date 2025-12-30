40ed7622a7183823e8ef5ef0e7ee4acc149dcfb5
param313136 <param313136@gmail.com>
2025-12-30 12:42:23 -0500

    commit message

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#addition, subtraction, multiplication and division program
+num1=int(input('enter first number:'))
+num2=int(input('enter second number:'))
+add = num1 + num2
+diff = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print('addition result:',add)
+print('subtraction result:', diff)
+print('multiplication result:',mult)
+print('division result',div)
+print ('end of program')
