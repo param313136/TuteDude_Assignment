954289b9fc1d6e6a23b215bc1b06f2c51fe58d4d
param313136 <param313136@gmail.com>
2025-12-30 13:02:31 -0500

    initial commit

---
+++ TEST.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+num1=int(input('enter first number:'))
+num2=int(input('enter second number:'))
+add = num1 + num2
+diff = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+
+print('addition result:',add,'substraction result:', diff, 'multiplication result:',mult,'division result',div)
+print ('end of program')
