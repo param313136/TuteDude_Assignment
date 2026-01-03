5092ba2b43f4e114374b1f133abd331aed629922
param313136 <param313136@gmail.com>
2026-01-03 12:19:21 -0500

    'message'

---
+++ Assign2_Control_Structures.py
@@ -0,0 +1,20 @@
+#Task#1
+'''
+# Python code to determine even or odd number
+num=int(input('Enter a number :'))
+if num%2 == 0 :
+    print(f'{num} is an even number.')
+else:
+    print(f'{num} is an odd number.')
+'''
+
+#Task#2
+
+# Python code to give sum of first 50 numbers
+
+sum = 0
+for i in range (0,51):
+    sum = sum + i
+
+print('The sum of numbers from 1 to 50 is :', sum)
+
